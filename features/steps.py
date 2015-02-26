44b8afb3f659d8335ba317e614e10944062ab315
Louis-Dominique Dubeau <ldd@lddubeau.com>
2015-02-25 20:35:34 -0500

    PEP8 cleanup.

--- features/steps.py
+++ features/steps.py
@@ -10,14 +10,16 @@ import multiprocessing
 import fcntl
 import six
 
-from fresher import * # pylint: disable=W0401,W0614
-from fresher.checks import * # pylint: disable=W0401,W0614
+from fresher import *  # pylint: disable=W0401,W0614
+from fresher.checks import *  # pylint: disable=W0401,W0614
+
 
 def decode(string):
     if not six.PY3:
         return string.decode('utf-8')
     else:
         return string if isinstance(string, str) else string.decode('utf-8')
+
 
 def glerbl_func(args, stdin, stdout, stderr):
     sys.stdin = os.fdopen(stdin, 'r')
@@ -30,12 +32,13 @@ def glerbl_func(args, stdin, stdout, stderr):
     sys.stderr.flush()
     exit(ret)
 
+
 def start_glerbl(args):
     (stdin_r, stdin_w) = os.pipe()
     (stdout_r, stdout_w) = os.pipe()
     (stderr_r, stderr_w) = os.pipe()
     ret = multiprocessing.Process(target=glerbl_func,
-                           args=[args, stdin_r, stdout_w, stderr_w])
+                                  args=[args, stdin_r, stdout_w, stderr_w])
     ret.start()
     os.close(stdin_r)
     os.close(stdout_w)
@@ -43,8 +46,10 @@ def start_glerbl(args):
     return (ret, os.fdopen(stdin_w, 'w'), os.fdopen(stdout_r, 'r'),
             os.fdopen(stderr_r, 'r'))
 
+
 def git(repo, *args):
     subprocess.check_call(args, cwd=repo)
+
 
 @contextlib.contextmanager
 def chdir(to):
@@ -52,6 +57,7 @@ def chdir(to):
     os.chdir(to)
     yield to
     os.chdir(pwd)
+
 
 @contextlib.contextmanager
 def temp_dir():
@@ -65,50 +71,59 @@ def temp_chdir():
         with chdir(tmpdir):
             yield tmpdir
 
+
 @After
 def after(_):
     if scc.repo is not None:
         shutil.rmtree(scc.repo)
 
+
 @Given("^a non-git directory$")
 def non_git():
     scc.repo = tempfile.mkdtemp(dir="features")
+
 
 @Given("^a git repository$")
 def git_step():
     scc.repo = tempfile.mkdtemp(dir="features")
     git(scc.repo, "git", "init", "-q")
 
+
 @Given("^a configuration that does not have the checks variable$")
 def config_without_checks():
     os.mkdir(os.path.join(scc.repo, ".glerbl"))
     open(os.path.join(scc.repo, ".glerbl", "repo_conf.py"), 'w').close()
+
 
 @Given("^a local_conf.py file$")
 def local_conf():
     os.mkdir(os.path.join(scc.repo, ".glerbl"))
     open(os.path.join(scc.repo, ".glerbl", "local_conf.py"), 'w').close()
 
+
 @Given("^a configuration that refers to a non-existent check$")
 def config_with_non_existent_check():
     os.mkdir(os.path.join(scc.repo, ".glerbl"))
     with open(os.path.join(scc.repo, ".glerbl", "repo_conf.py"), 'w') as \
-         conf:
+            conf:
         conf.write("checks = {'pre-commit': ['nonexistent']}\n")
+
 
 @Given("^a configuration that refers to a check for the wrong hook$")
 def config_with_check_for_wrong_hook():
     os.mkdir(os.path.join(scc.repo, ".glerbl"))
     with open(os.path.join(scc.repo, ".glerbl", "repo_conf.py"), 'w') as \
-         conf:
+            conf:
         conf.write("checks = {'post-commit': ['no_before_commit']}\n")
+
 
 @Given("^a correct configuration$")
 def correct_configuration():
     os.mkdir(os.path.join(scc.repo, ".glerbl"))
     with open(os.path.join(scc.repo, ".glerbl", "repo_conf.py"), 'w') as \
-         conf:
+            conf:
         conf.write("checks = {'pre-commit': ['no_before_commit']}\n")
+
 
 @Given("^a staged file$")
 def a_staged_file():
@@ -122,7 +137,7 @@ def git_repo_in_with_x(check):
     run_steps("Given a git repository")
     os.mkdir(os.path.join(scc.repo, ".glerbl"))
     with open(os.path.join(scc.repo, ".glerbl", "repo_conf.py"), 'w') as \
-         conf:
+            conf:
         conf.write("checks = {{{0}}}\n"
                    .format("" if check == "no checks are"
                            else ("'pre-commit': ['{0}']".format(check[0:-3]))))
@@ -143,7 +158,7 @@ def git_repo_in_with_x(check):
 @When(r"^the user removes all checks$")
 def user_removes_all_checks():
     with open(os.path.join(scc.repo, ".glerbl", "repo_conf.py"), 'w') as \
-         conf:
+            conf:
         conf.write("checks = {}\n")
 
 
@@ -158,7 +173,7 @@ def git_repo_in_with_all_pre_commit():
 
     os.mkdir(os.path.join(scc.repo, ".glerbl"))
     with open(os.path.join(scc.repo, ".glerbl", "repo_conf.py"), 'w') as \
-         conf:
+            conf:
         conf.write("checks = {{'pre-commit': {0}}}\n".format(repr(checks)))
     run_steps("""
     When the user installs glerbl
@@ -172,10 +187,12 @@ def git_repo_in_with_all_pre_commit():
     del scc.stderr
     del scc.stdout
 
+
 @Given("^([^ ]*?) exists$")
 def x_exists(x):
     with open(os.path.join(scc.repo, x), 'w'):
         pass
+
 
 @Given("^a non-ascii filename is staged$")
 def non_ascii_exists():
@@ -183,17 +200,20 @@ def non_ascii_exists():
         pass
     git(scc.repo, "git", "add", u"ālaya")
 
+
 @Given("^a file with trailing whitespace is staged$")
 def file_with_trailing_whitespace_staged():
     with open(os.path.join(scc.repo, u"foo"), 'w') as foo:
         foo.write("a = 'blah'  ")
     git(scc.repo, "git", "add", u"foo")
 
+
 @Given("^a python file with a trailing semicolon is staged$")
 def python_file_with_trailing_semicolon_staged():
     with open(os.path.join(scc.repo, u"foo.py"), 'w') as foo:
         foo.write("a = 'blah';")
     git(scc.repo, "git", "add", u"foo.py")
+
 
 @Given("^a python file that violates PEP8 is staged$")
 def python_file_that_violates_pep8_staged():
@@ -206,9 +226,10 @@ def python_file_that_violates_pep8_staged():
 def hooks_allownonascii_true():
     git(scc.repo, "git", "config", "hooks.allownonascii", "true")
 
+
 @Given(r"^environment variable (\w+) is set$")
 def environment_variable_x_is_set(x):
-    os.environ[x]="1"
+    os.environ[x] = "1"
 
 
 @Given(r"^a staged deleted file which would normally be checked and "
@@ -257,10 +278,12 @@ def user_installs_glerbl():
         scc.stderr = stderr
         scc.stdout = stdout
 
+
 @When("^answers affirmatively the install prompt$")
 def user_answers_affirmatively():
     scc.stdin.write("yes\n")
     scc.stdin.flush()
+
 
 @When("^the user commits$")
 def the_user_commits():
@@ -274,7 +297,8 @@ def the_user_commits():
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE, cwd=scc.repo)
     (scc.git_stdout, scc.git_stderr) = proc.communicate()
-    scc.git_returncode = proc.returncode # pylint: disable=E1101
+    scc.git_returncode = proc.returncode  # pylint: disable=E1101
+
 
 @Then("^glerbl prompts the user$")
 def glerbl_prompts_the_user():
@@ -284,6 +308,7 @@ def glerbl_prompts_the_user():
         decode(scc.stdout.read().strip()),
         "This will overwrite your current hooks. Proceed [yes/No]?")
 
+
 @Then('^glerbl fails with "(.*?)"$')
 def glerbl_fails_with(value):
     scc.proc.join()
@@ -291,23 +316,26 @@ def glerbl_fails_with(value):
                  value.strip().replace(r'\n', '\n'))
     assert_equal(scc.proc.exitcode, 1)
 
+
 @Then("^glerbl installs the hooks$")
 def glerbl_installs_the_hooks():
     scc.proc.join()
     commit_hook_path = os.path.join(scc.repo, ".git/hooks/pre-commit")
     assert_true(os.path.exists(commit_hook_path))
     commit_hook = open(commit_hook_path, 'r')
-    commit_hook.readline() # skip first line
+    commit_hook.readline()  # skip first line
     assert_equal(commit_hook.readline().strip(),
-                "# GENRATED BY GLERBL. DO NOT MODIFY.")
+                 "# GENRATED BY GLERBL. DO NOT MODIFY.")
     assert_equal(scc.proc.exitcode, 0)
     commit_hook.close()
+
 
 @Then(u'^the commit fails with "(.*?)"$')
 def commit_fails_with(value):
     assert_equal(scc.git_returncode, 1)
     assert_equal(decode(scc.git_stderr),
                  value.strip().replace(r'\n', '\n'))
+
 
 @Then(u'^the commit fails with trailing "(.*?)"$')
 def commit_fails_with_trailing(value):
